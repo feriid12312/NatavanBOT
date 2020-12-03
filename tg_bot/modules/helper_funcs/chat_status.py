9fbe46e6f4715e22cf3c7463bb6b9bd1cad8e0b4
ValiyevAli <69925189+ValiyevAli@users.noreply.github.com>
2020-12-04 02:36:05 +0400

    Update chat_status.py

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -54,8 +54,8 @@ def bot_can_delete(func):
         if can_delete(update.effective_chat, bot.id):
             return func(bot, update, *args, **kwargs)
         else:
-            update.effective_message.reply_text("I can't delete messages here! "
-                                                "Make sure I'm admin and can delete other user's messages.")
+            update.effective_message.reply_text("Mesajları silə bilmirəm!"
+                                                "Admin olduğumdan və ya mesaj silmə funksiyamın açıq olduğundan əmin ol.")
 
     return delete_rights
 
@@ -66,8 +66,8 @@ def can_pin(func):
         if update.effective_chat.get_member(bot.id).can_pin_messages:
             return func(bot, update, *args, **kwargs)
         else:
-            update.effective_message.reply_text("I can't pin messages here! "
-                                                "Make sure I'm admin and can pin messages.")
+            update.effective_message.reply_text("Mesajları sabitliyə bilmirəm! "
+                                                "Admin olduğumdan və ya mesaj sabitləmə funksiyamın açıq olduğundan əmin ol.")
 
     return pin_rights
 
@@ -78,8 +78,8 @@ def can_promote(func):
         if update.effective_chat.get_member(bot.id).can_promote_members:
             return func(bot, update, *args, **kwargs)
         else:
-            update.effective_message.reply_text("I can't promote/demote people here! "
-                                                "Make sure I'm admin and can appoint new admins.")
+            update.effective_message.reply_text("Mən bunu edə bilməyəcəm "
+                                                "Yeni admin əlavə etmə funksiyamın açıq olduğundan əmin ol")
 
     return promote_rights
 
@@ -90,8 +90,8 @@ def can_restrict(func):
         if update.effective_chat.get_member(bot.id).can_restrict_members:
             return func(bot, update, *args, **kwargs)
         else:
-            update.effective_message.reply_text("I can't restrict people here! "
-                                                "Make sure I'm admin and can appoint new admins.")
+            update.effective_message.reply_text("Buradakı insanları məhdudlaşdıra bilmərəm!"
+                                                "Admin olduğuma və yeni admin təyin edə biləcəyinə əmin olun.")
 
     return promote_rights
 
@@ -102,7 +102,7 @@ def bot_admin(func):
         if is_bot_admin(update.effective_chat, bot.id):
             return func(bot, update, *args, **kwargs)
         else:
-            update.effective_message.reply_text("I'm not admin!")
+            update.effective_message.reply_text("Mən admin deyiləm!")
 
     return is_admin
 
@@ -121,7 +121,7 @@ def user_admin(func):
             update.effective_message.delete()
 
         else:
-            update.effective_message.reply_text("Who dis non-admin telling me what to do?")
+            update.effective_message.reply_text("Admin olmayan adam mənə nə edəcəyimi deyir? Çox gülüncsən))")
 
     return is_admin
 
