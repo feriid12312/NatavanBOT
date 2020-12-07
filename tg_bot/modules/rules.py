e5aa6fedab6f2ba53b64f9ab182652808127f601
Ali Valiyev <69925189+ValiyevAli@users.noreply.github.com>
2020-12-07 22:34:58 +0400

    Update rules.py

--- tg_bot/modules/rules.py
+++ tg_bot/modules/rules.py
@@ -33,7 +33,7 @@ def send_rules(update, chat_id, from_pm=False):
             raise
 
     rules = sql.get_rules(chat_id)
-    text = "The rules for *{}* are:\n\n{}".format(escape_markdown(chat.title), rules)
+    text = "*{}* üçün qaydalar:\n\n{}".format(escape_markdown(chat.title), rules)
 
     if from_pm and rules:
         bot.send_message(user.id, text, parse_mode=ParseMode.MARKDOWN)
@@ -43,7 +43,7 @@ def send_rules(update, chat_id, from_pm=False):
     elif rules:
         update.effective_message.reply_text("Bu qrupun qaydalarını öyrənmək üçün PM-də mənimlə əlaqə saxlayın.",
                                             reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="Rules",
+                                                [[InlineKeyboardButton(text="Qaydalar",
                                                                        url="t.me/{}?start={}".format(bot.username,
                                                                                                      chat_id))]]))
     else:
